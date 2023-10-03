9e012d0a532242931eff7853fa7739a0ac8fe326
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2023-10-04 00:38:17 +0300

    added GET and POST features for hikes

---
+++ login/models.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import validates
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# defining the models
+class UserSignup(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(), nullable=False, unique=True)
+    last_name = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), nullable=False)
+    password = db.Column(db.String(), nullable=False)    
+    username = db.Column(db.String(), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False, unique=True)
+    user_id = db.Column(db.Integer(), nullable=False, unique=True)
+    hike_id = db.Column(db.Integer(), nullable=False, unique=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+class Hike(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String(), nullable=False, unique=True)
+   description = db.Column(db.String(), nullable=False)
+   distance = db.Column(db.String(), nullable=False)
+   difficulty =db.Column(db.String(), nullable=False)
