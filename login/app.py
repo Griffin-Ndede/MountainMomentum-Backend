62521e02a7e1b189d1ab1421e7a096c3e641240e
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2023-10-04 21:27:26 +0300

    finalized on the login and register routes

--- login/app.py
+++ login/app.py
@@ -7,7 +7,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # creating a flask application instance
 app = Flask(__name__)
 # specifying database file name
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:/// mountainmomentum.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///mountainmomentum.db"
 
 # initializing the database with flask application
 db.init_app(app)
@@ -57,16 +57,16 @@ def hikes():
         ]
         return jsonify(hiking_json)
     else:
-        return "Invalid request method"
+        return "Invalid request method", 400
     
 @app.route("/login", methods= ['GET', 'POST'])
 def login():
     if request.method == "POST" and "username" in request.form and "password" in request.form:
         username = request.form['username']
         password = request.form['password']
-        user = UserSignup.query.filter_by(username=username, password=password).first()
+        user = UserSignup.query.filter_by(username=username).first()
 
-        if user and check_password_hash (user.password_hash, password):
+        if user and check_password_hash (user.password, password):
             session["loggedin"] = True
             session["id"]= user.id
             session["username"] = user.username
@@ -76,7 +76,7 @@ def login():
             # redirect to a different route
             return redirect(url_for("home"))
         else:
-            return "Incorrect username/password!", "error"
+            return "Incorrect username/password!", 400
 
 @app.route('/logout')
 def logout():
@@ -88,6 +88,8 @@ def logout():
 @app.route('/register', methods = ['GET', "POST"])
 def register():
     if request.method == "POST":
+
+        # retrieving the data from the form
         username =request.form["username"]
         password = request.form["password"]
         email = request.form["email"]
@@ -99,23 +101,23 @@ def register():
         email_exists = UserSignup.query.filter_by(email=email).first()
 
         if user_exists:
-            return "Account already exists", "error"
+            return "Account already exists", 400
         elif email_exists:
-            return "Email already exists", "error"
+            return "Email already exists", 400
         elif not username or not password or not email or not first_name or not last_name:
             return "Please fill out the entire form", "error"
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email) or not re.match(r'[A-Za-z0-9]+', username):
             return "Invalid email address or username", "error"
         else:
         # hashing the password before storing it in the database
-            password_hash = generate_password_hash(password, method = "sha256")
-            new_user = UserSignup(username=username, password=password_hash, email=email, first_name=first_name, last_name=last_name)
+            password = generate_password_hash(password, method="sha256")
+            new_user = UserSignup(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
             db.session.add(new_user)
             db.session.commit()
 
         return "You have successfully registered!"
 
-    # Redirect the user, whether the registration was successful or not
+    # Redirect the user, if the registration is successful
     return redirect(url_for("home"))
 
 
